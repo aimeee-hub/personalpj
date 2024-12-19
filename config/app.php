0a4d879b63d64e05344d47595fe17df6f8184cce
“aimeee-hub <174617670+aimeee-hub@users.noreply.github.com>
2024-12-19 19:59:23 +0900

    AppServiceProvider修正

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://your-heroku-app.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL'),
 
